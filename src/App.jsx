e8a729ed1fc73f6c3b01f385a88b0678df63d652
Kidus Yosef <148970622+kid-yP@users.noreply.github.com>
2025-12-08 07:06:12 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import {Excalidraw} from "@excalidraw/excalidraw"
+
+function App() {
+  return(
+    <>
+      <div style={{ height: "100vh" }}>
+        <Excalidraw/>     
+      </div>
+    </>
+  )
+}
+
+export default App
